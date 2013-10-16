9e0620553f414a1edf97638389894b40924c42a1
mkwiek <maciej.iai@gmail.com>
2013-10-16 22:17:43 +0200

    Added paging, changed names

--- app.js
+++ app.js
@@ -6,23 +6,25 @@ app.use(express.bodyParser())
 
 var db = mongoskin.db('localhost:27017/test', {safe:true});
 
+var pageSize = 10
+
 app.param('collectionName', function(req, res, next, collectionName){
   req.collection = db.collection(collectionName)
   return next()
 })
 
 app.get('/', function(req, res, next) {
-  res.send('please select a collection, e.g., /collections/messages')
+  res.send('please select a collection, e.g., /api/messages')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+app.get('/api/:collectionName', function(req, res, next) {
+  req.collection.find({},{limit:pageSize, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.post('/collections/:collectionName', function(req, res, next) {
+app.post('/api/:collectionName', function(req, res, next) {
   req.collection.insert(req.body, {}, function(e, results){
     if (e) return next(e)
     res.send(results)
@@ -30,25 +32,39 @@ app.post('/collections/:collectionName', function(req, res, next) {
 })
 
 
-app.get('/collections/:collectionName/:id', function(req, res, next) {
+app.get('/api/:collectionName/:id', function(req, res, next) {
   req.collection.findOne({_id: req.collection.id(req.params.id)}, function(e, result){
     if (e) return next(e)
     res.send(result)
   })
 })
-app.put('/collections/:collectionName/:id', function(req, res, next) {
+app.put('/api/:collectionName/:id', function(req, res, next) {
   req.collection.update({_id: req.collection.id(req.params.id)}, {$set:req.body}, {safe:true, multi:false}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
 })
-app.del('/collections/:collectionName/:id', function(req, res, next) {
+app.del('/api/:collectionName/:id', function(req, res, next) {
   req.collection.remove({_id: req.collection.id(req.params.id)}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
 })
 
+app.get('/api/:collectionName/page/:pageNumber', function(req, res, next) {
+  req.collection.find({},{skip: pageSize * (req.params.pageNumber - 1), limit:pageSize, sort: [['_id',-1]]}).toArray(function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.del('/api/:collectionName', function(req,res,next){
+  req.collection.remove({},{},function()
+    {
+      res.send({msg:"removed " + req.params.collectionName})
+    })
+})
 
 
+console.log("listening on 3000")
 app.listen(3000)
--- test.js
+++ test.js
@@ -5,9 +5,8 @@ describe('express rest api server', function(){
   var id
 
   it('post object', function(done){
-    superagent.post('http://localhost:3000/collections/test')
+    superagent.post('http://localhost:3000/api/users')
       .send({ name: 'John'
-        , email: 'john@rpjs.co'
       })
       .end(function(e,res){
         // console.log(res.body)
@@ -20,9 +19,9 @@ describe('express rest api server', function(){
   })
 
   it('retrieves an object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+    superagent.get('http://localhost:3000/api/users/'+id)
       .end(function(e, res){
-        // console.log(res.body)
+        //console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body._id.length).to.eql(24)
@@ -32,7 +31,7 @@ describe('express rest api server', function(){
   })
 
   it('retrieves a collection', function(done){
-    superagent.get('http://localhost:3000/collections/test')
+    superagent.get('http://localhost:3000/api/users')
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -43,9 +42,8 @@ describe('express rest api server', function(){
   })
 
   it('updates an object', function(done){
-    superagent.put('http://localhost:3000/collections/test/'+id)
-      .send({name: 'Peter'
-        , email: 'peter@yahoo.com'})
+    superagent.put('http://localhost:3000/api/users/'+id)
+      .send({name: 'Peter'})
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -56,7 +54,7 @@ describe('express rest api server', function(){
   })
 
   it('checks an updated object', function(done){
-    superagent.get('http://localhost:3000/collections/test/'+id)
+    superagent.get('http://localhost:3000/api/users/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
@@ -68,12 +66,64 @@ describe('express rest api server', function(){
       })
   })
   it('removes an object', function(done){
-    superagent.del('http://localhost:3000/collections/test/'+id)
+    superagent.del('http://localhost:3000/api/users/'+id)
       .end(function(e, res){
         // console.log(res.body)
         expect(e).to.eql(null)
         expect(typeof res.body).to.eql('object')
         expect(res.body.msg).to.eql('success')
+        done()
+      })
+  })
+
+  it('checks if object is removed', function(done){
+    superagent.get('http://localhost:3000/api/users/'+id)
+      .end(function(e, res){
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body._id).to.eql(undefined)
+        done()
+      })
+  })
+
+  it('adds 15 new users and checks if pagination works as expected', function(done){
+    var callback = function (e, res, i) 
+    { 
+      // console.log("calling callback with " + i)
+      if (i < 15) {
+        superagent.post('http://localhost:3000/api/users')
+          .send({ name: 'user number ' + i}).end(function(e,res){
+            callback(e,res,i+1)
+          }) 
+      }else{
+        superagent.get('http://localhost:3000/api/users/page/1')
+        .end(function(e, res){
+          // console.log(res.body)
+          expect(e).to.eql(null)
+          expect(res.body.length).to.be.equal(10)
+
+          superagent.get('http://localhost:3000/api/users/page/2')
+          .end(function(e, res){
+            // console.log(res.body)
+            expect(e).to.eql(null)
+            expect(res.body.length).to.be.equal(5)
+            done()
+          })
+        })
+      }
+    }
+
+    callback(null, null, 0)
+
+  })
+
+  it('cleans up after tests', function(done){
+    superagent.del('http://localhost:3000/api/users')
+      .end(function(e, res){
+        // console.log(res.body)
+        expect(e).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body.msg).to.eql('removed users')
         done()
       })
   })
