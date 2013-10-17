d01245158f169486a189c74fab0b2359acbe3631
mkwiek <maciej.iai@gmail.com>
2013-10-18 00:58:38 +0200

    Basic List view

--- public/js/views/List.js
+++ public/js/views/List.js
@@ -1,6 +1,33 @@
-browserver.views.List = Backbone.View.extend({
-  tagName: 'ul', // required, but defaults to 'div' if not set
-  className: 'container', // optional, you can assign multiple classes to 
-                          // this property like so: 'container homepage'
-  id: 'todos', // optional
+$(document).ready(function(){
+  browserver.views.List = Backbone.View.extend({
+
+    initialize: function(options){ 
+      $.extend(this,options);
+      var that = this;
+      _.bindAll(that,'renderList');
+      that.collection.bind('reset',that.renderList);
+      that.render();
+      that.loadData();
+    },
+
+    render: function() {
+      var that = this;
+      $(that.el).html(that.template());
+      return that;
+    },
+
+    renderList: function(){
+      var that = this; 
+      _.each(that.collection.toJSON(),function(item){
+        $(that.el).append(that.list_item_template(item));
+      });
+
+    },
+
+    loadData : function(){
+      var that = this;
+      that.collection.fetch({reset:true});
+    }
+                 
+  });
 });
