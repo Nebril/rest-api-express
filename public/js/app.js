33039c450d7530c056069d8c49654fd675e95012
mkwiek <maciej.iai@gmail.com>
2013-10-18 00:59:59 +0200

    Loading scripts and creating views

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,21 @@ browserver.Router = Backbone.Router.extend({
 
 });
 
-$(document).on("ready", function () {
+$(document).ready(function () {    
+    var usersView = new browserver.views.List({
+        el : $("#userList"),
+        collection : new browserver.models.UserCollection(),
+        template : _.template("<ul></ul>"),
+        list_item_template : _.template("<li><%= name %></li>")
+    });
+
+    var serversView = new browserver.views.List({
+        el : $("#serverList"),
+        collection : new browserver.models.ServerCollection(),
+        template : _.template("<ul></ul>"),
+        list_item_template : _.template("<li><%= name %></li>")
+    });
+
     browserver.router = new browserver.Router();
     Backbone.history.start();
 });
